2d878542ada17e4c647dcf7c2793aafd4c395e3e
tony <xcc2533919@163.com>
2018-01-27 17:58:07 +0800

    Update test_02.go

--- go/test_02.go
+++ go/test_02.go
@@ -34,28 +34,28 @@ func main() {
 		f3()
 	}
 	// for 初始化语句; 条件语句; 修饰语句 {} 其中初始化语句/修饰语句可以省略
-  for i, j := 10, 20; i < 10; i++ {
+  	for i, j := 10, 20; i < 10; i++ {
   
-  }
-  // for语句块, 无限循环, 在语句块里控制退出
-  for {
-    i:=1
-    i++
-    if i > 10 {
-      return    // 直接返回, 退出函数
-      // break  退出该次循环体
-    }
-  }
+  	}
+  	// for语句块, 无限循环, 在语句块里控制退出
+	  for {
+	    i:=1
+	    i++
+	    if i > 10 {
+	      return    // 直接返回, 退出函数
+	      // break  退出该次循环体
+	    }
+	  }
   
-  for i < 10 {
-  }
-  // for-range
-  for pos, char := range str {
-  }
-  
-  // label可配合for/switch/select结构
-  goto LABEL  //保证label在goto之后
-  continue LABEL //跳过下面的语句回到LABEL标签处
-  break LABEL  //直接退出循环
+	  for i < 10 {
+	  }
+	  // for-range
+	  for pos, char := range str {
+	  }
+
+	  // label可配合for/switch/select结构
+	  goto LABEL  //保证label在goto之后
+	  continue LABEL //跳过下面的语句回到LABEL标签处
+	  break LABEL  //直接退出循环
 
 }
