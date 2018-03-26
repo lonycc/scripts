a71d65cbccb02e4d97d7ad0cd57c37972b55daa8
tony <xcc2533919@163.com>
2018-03-26 15:08:42 +0800

    Update router.go

--- go/gin_rest/router.go
+++ go/gin_rest/router.go
@@ -12,8 +12,12 @@ func main() {
 
 	// 使用中间件, 每次路由请求都会调用
 	router.Use(Middleware)
+	router.Use(gin.Logger())
+	router.Use(gin.Recovery())
 
 	v1 := router.Group("/api/v1/person")
+	// 只对该分组应用中间件
+	// v1.Use(xxMiddleware.MiddlewareFunc())
 	{
 		v1.POST("/", AddPerson)
 		v1.GET("/", GetPersons)
