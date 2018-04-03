e3cd7d161521938e51bb240781d1e4419463a9f3
tony <xcc2533919@163.com>
2018-04-03 14:48:25 +0800

    Update upstream.go

--- go/echo_demo/upstream.go
+++ go/echo_demo/upstream.go
@@ -9,6 +9,7 @@ import (
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	"golang.org/x/net/websocket"
+	"github.com/gorilla/websocket" g_websocket
 )
 
 var index = `
@@ -43,6 +44,30 @@ var index = `
 	</body>
 	</html
 `
+var upgrader = g_websocket.Upgrader{}
+
+func ws_gorilla(c echo.Context) error {
+	ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
+	if err != nil {
+		return err
+	}
+	defer ws.Close()
+
+	for {
+		// 向客户端发送
+		err := ws.WriteMessage(websocket.TextMessage, []byte("Hello, Client!"))
+		if err != nil {
+			c.Logger().Error(err)
+		}
+
+		// 接收客户端信息
+		_, msg, err := ws.ReadMessage()
+		if err != nil {
+			c.Logger().Error(err)
+		}
+		fmt.Printf("%s\n", msg)
+	}	
+}
 
 func main() {
 	name := os.Args[1]
@@ -54,21 +79,32 @@ func main() {
 		return c.HTML(http.StatusOK, fmt.Sprintf(index, name))
 	})
 
-	// WebSocket handler
+	// WebSocket handler from golang
 	e.GET("/ws", func(c echo.Context) error {
 		websocket.Handler(func(ws *websocket.Conn) {
 			defer ws.Close()
 			for {
-				// Write
+				// 向客户端发送
 				err := websocket.Message.Send(ws, fmt.Sprintf("Hello from upstream server %s!", name))
 				if err != nil {
 					e.Logger.Error(err)
 				}
 				time.Sleep(1 * time.Second)
+				
+				// 接收客户端信息
+				msg := ""
+				err = websocket.Message.Read(ws, &msg)
+				if err != nil {
+					c.Logger().Error(err)
+				}
+				fmt.Printf("%s\n", msg)
 			}
 		}).ServeHTTP(c.Response(), c.Request())
 		return nil
 	})
+	
+	// websocket handler from gorilla
+	e.GET("/ws_gorilla", ws_gorilla)
 
 	e.Logger.Fatal(e.Start(port))
 }
