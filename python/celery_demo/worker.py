eca7d5a32a07b12d3eb4b84c30c98ce8fb741114
tony <xcc2533919@163.com>
2018-04-13 16:04:01 +0800

    Create worker.py

---
+++ python/celery_demo/worker.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+
+import os
+import redis
+from rq import Worker, Queue, Connection
+
+listen = ['high', 'default', 'low']
+redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
+conn = redis.from_url(redis_url)
+
+if __name__ == '__main__':
+    with Connection(conn):
+        worker = Worker(map(Queue, listen))
+        worker.work()
