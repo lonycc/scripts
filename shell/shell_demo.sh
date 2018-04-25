2880bb87f7268e2a30917c08fa96990d8894e532
tony <xcc2533919@163.com>
2018-04-25 12:34:13 +0800

    Update shell_demo.sh

--- shell/shell_demo.sh
+++ shell/shell_demo.sh
@@ -253,3 +253,28 @@ iptables -A INPUT -s 127.0.0.1 -d 127.0.0.1 -j ACCEPT #允许本地回环
 iptables -A OUTPUT -j ACCEPT  #允许本机对外访问
 iptables -A INPUT -s xx.xx.xx.xx -j DROP #永久屏蔽单个ip
 iptables -I INPUT -s 192.0.0.0/8 -j DROP #屏蔽192段ip
+
+
+#定义颜色的变量
+RED_COLOR='\E[1;31m'  #红
+GREEN_COLOR='\E[1;32m' #绿
+YELOW_COLOR='\E[1;33m' #黄
+BLUE_COLOR='\E[1;34m'  #蓝
+PINK='\E[1;35m'      #粉红
+RES='\E[0m'
+#需要使用echo -e
+echo -e  "${RED_COLOR}======red color======${RES}"
+echo -e  "${YELOW_COLOR}======yelow color======${RES}"
+echo -e  "${BLUE_COLOR}======green color======${RES}"
+echo -e  "${GREEN_COLOR}======green color======${RES}"
+echo -e  "${PINK}======pink color======${RES}"
+echo "#############################################################"
+#直接把echo -e放到变量里面，使用的时候直接输出变量即可
+SETCOLOR_SUCCESS="echo -en \\033[1;32m"
+SETCOLOR_FAILURE="echo -en \\033[1;31m"
+SETCOLOR_WARNING="echo -en \\033[1;33m"
+SETCOLOR_NORMAL="echo -en \\033[0;39m"
+echo ----oldboy trainning-----  &&  $SETCOLOR_SUCCESS
+echo ----oldboy trainning-----  &&  $SETCOLOR_FAILURE
+echo ----oldboy trainning-----  &&  $SETCOLOR_WARNING
+echo ----oldboy trainning-----  &&  $SETCOLOR_NORMAL
