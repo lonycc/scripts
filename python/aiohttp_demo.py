a50203b924c63136c812c2160bf55dc5935aad6f
tony <xcc2533919@163.com>
2018-08-07 11:58:04 +0800

    Update aiohttp_demo.py

--- python/aiohttp_demo.py
+++ python/aiohttp_demo.py
@@ -35,7 +35,34 @@ async def main():
                     if not chunk:
                         break
                     fd.write(chunk)
+
+
+# 文件发送
+async def file_sender(file_name=None):
+    async with aiofiles.open(file_name, 'rb') as f:
+        chunk = await f.read(64*1024)
+        while chunk:
+            yield chunk
+            chunk = await f.read(64*1024)
             
+# 上传文件/流
+async def stream_uploads():
+    async with aiohttp.ClientSession() as session:
+        # 显示指定filename和content_type
+        data = FormData()
+        data.add_field('file', open('report.xls', 'rb'), filename='report.xls', content_type='application/vnd.ms-excel')
+        await session.post('http://httpbin.org/post', data=data)
+        
+        # 大文件
+        with open('bigtext.txt', 'rb') as f:
+            await session.post('http://httpbin.org/post', data=f)
+        
+        # 流式发送大文件
+        async with session.post('http://httpbin.org/post', data=file_sender(file_name='huge_file')) as resp:
+            print(await resp.text())
+        
+        resp = await session.get('http://python.org')
+        await session.post('http://httpbin.org/post', data=resp.content)
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
