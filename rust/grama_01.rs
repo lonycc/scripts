c2d3fcbbe2d0e42a27755561e3e5c4e01159b719
tony <xcc2533919@163.com>
2019-08-11 14:21:41 +0800

    Update grama_01.rs

--- rust/grama_01.rs
+++ rust/grama_01.rs
@@ -1,6 +1,20 @@
 use ferris_says::say;
 use std::io::{stdout, BufWriter};
 use std::cell::Cell;
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
+
+static LOREM_IPSUM: &'static str =
+"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+";
 
 #[derive(Default)]
 struct Point3d {
@@ -47,12 +61,60 @@ fn main() {
 }
 
 fn ferris() {
-    let stdout = stdout();
+    let stdout = stdout(); // 标准输出
     let out = b"Hello fellow Rustaceans!";
     let width = 24;
 
     let mut writer = BufWriter::new(stdout.lock());
     say(out, width, &mut writer).unwrap();
+}
+
+
+fn test_file_reader() {
+    // 创建一个文件路径
+    let path = Path::new("hello.txt");
+    let display = path.display();
+
+    // 打开文件只读模式, 返回一个 `io::Result<File>` 类型
+    let mut file = match File::open(&path) {
+        // 处理打开文件可能潜在的错误
+        Err(why) => panic!("couldn't open {}: {}", display, Error::description(&why)),
+        Ok(file) => file,
+    };
+
+    // 文件输入数据到字符串，并返回 `io::Result<usize>` 类型
+    let mut s = String::new();
+    match file.read_to_string(&mut s) {
+        Err(why) => panic!("couldn't read {}: {}", display, Error::description(&why)),
+        Ok(_) => print!("{} contains:\n{}", display, s),
+    }
+}
+
+fn test_file_writer() {
+    let path = Path::new("out/lorem_ipsum.txt");
+    let display = path.display();
+
+    // 用只写模式打开一个文件，并返回 `io::Result<File>` 类型
+    let mut file = match File::create(&path) {
+        Err(why) => panic!("couldn't create {}: {}", display, Error::description(&why)),
+        Ok(file) => file,
+    };
+
+    // 写入 `LOREM_IPSUM` 字符串到文件中, 并返回 `io::Result<()>` 类型
+    match file.write_all(LOREM_IPSUM.as_bytes()) {
+        Err(why) => {
+            panic!("couldn't write to {}: {}", display, Error::description(&why))
+        },
+        Ok(_) => println!("successfully wrote to {}", display),
+    }
+}
+
+fn test_std_io() {
+    let mut input = String::new();
+    print!("enter a string：");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut input).expect("WTF!");
+    println!("You typed: {}", input.trim());
 }
 
 fn array_test() {
