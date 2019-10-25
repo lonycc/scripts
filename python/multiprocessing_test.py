2a53f29e32f9c1733851e6e8e8ff597f9e731556
tony <xcc2533919@163.com>
2019-10-25 16:28:17 +0800

    增加进程队列

--- python/multiprocessing_test.py
+++ python/multiprocessing_test.py
@@ -1,6 +1,10 @@
 # coding:utf-8
 
+# 进程池中进程间通信使用multiprocessing.Manager().Queue()
+
 import multiprocessing
+import os
+import time
 
 def f1(name):
     print(name)
@@ -8,7 +12,7 @@ def f1(name):
 def f2(x):
     return x**2
 
-def test_multi_process():
+def test_1():
     p = multiprocessing.Process(target=f1, args=('tony',))
     p.start()
     p.join()
@@ -22,3 +26,27 @@ def test_multi_process():
     p2.close()  # 关闭pool, 不再接受新任务
     p2.join()  # 主进程阻塞, 等待子进程执行完成
     # p2.terminate()  # 立即终止
+
+def write(q):
+    print("write启动(%s)，父进程为(%s)" % (os.getpid(), os.getppid()))
+    for i in "python":
+        q.put(i)
+
+def read(q):
+    print("read启动(%s)，父进程为(%s)" % (os.getpid(), os.getppid()))
+    for i in range(q.qsize()):
+        print("read从Queue获取到消息：%s" % q.get(True))
+
+if __name__ == '__main__':
+    print("(%s) start" % os.getpid())
+    q = multiprocessing.Manager().Queue()
+    po = multiprocessing.Pool()
+    po.apply_async(write, args=(q,))
+
+    time.sleep(2)   
+    
+    po.apply_async(read, args=(q,))
+    po.close()
+    po.join()
+    
+    print("(%s) end" % os.getpid())  
